6f9644c107b8f97e69ff1755bcee770cc917b09e
zerowst <songtwang@163.com>
2024-12-24 02:28:08 +0000

    sim_figure

--- sim_checkpoints/polIter_rho1bSft2_vineppo_GSM8K/evaluation/figure.py
+++ sim_checkpoints/polIter_rho1bSft2_vineppo_GSM8K/evaluation/figure.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import glob
 import os
 import json
@@ -8,28 +8,48 @@ currect_dir = os.getcwd()
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 files = glob.glob(script_dir + "/ckpt*")
-print(files)
-
+# print(files)
 target_file = 'log.json'
 
-for file in files:
-    root_dir = file + '/gsm8k_test'
-    for root, dirs, files_ in os.walk(root_dir):
-        if target_file in files_:
-            full_path = os.path.join(root, target_file)
-            print(f"Found: {full_path}")
-            break
-    else: 
-        print('file not found')
-    
-    with open(full_path, 'r') as f:
 
-        res = json.load(f)
-    correct_acc = res['correct_frac']
-    print(correct_acc)
+def get_acc():
+    correct_acc = []
+    for file in files:
+        root_dir = file + '/gsm8k_test'
+        for root, dirs, files_ in os.walk(root_dir):
+            if target_file in files_:
+                full_path = os.path.join(root, target_file)
+                print(f"Found: {full_path}")
+                break
+        else: 
+            print('file not found')
+        
+        with open(full_path, 'r') as f:
+
+            res = json.load(f)
+        correct_acc.append(res['correct_frac'])
+        print(correct_acc)
+    return correct_acc
 
 
+def figure(points, axis):
+    plt.plot(axis, points, label='similarity', marker='o')
 
+    plt.xlabel('Training steps')
+    plt.ylabel('Correct_accuracy')
+    plt.title('Training performance of similarity method')
+    plt.legend()
+    plt.grid(True)
+
+    plt.show()
+    plt.savefig('acc.pdf')
+
+
+if __name__ == '__main__':
+    axis = range(10, 161, 10)
+    acc_points = get_acc()
+
+    figure(points=acc_points, axis=axis)
 
 
 
